19e07fb1f28ad212f4b068e657d03f61e1ad2ab9
James Tavernor <jtavernor79@gmail.com>
2017-09-20 18:38:47 +0100

    added a basic tasks page

---
+++ Tasks/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../bootstrap.css">
+    <title>Programming Tasks</title>
+</head>
+<?php
+include '../header.html';
+?>
+<body>
+<div class="container">
+    <div class="row">This is a collection of programming tasks I set myself just to tackle for fun</div>
+    <?php
+    $filesInDirectory = array_diff(scandir('.'), array('..', '.'));
+    foreach ($filesInDirectory as $currentFile) {
+        if (is_dir($currentFile)) {
+            //every directory inside this directory we scanned is a project so display it with a link to it
+            echo '<div class="row"></div><a href="' . $currentFile . '">' . $currentFile . '</a></div>';
+        }
+    }
+    ?>
+</div>
+</body>
+</html>
