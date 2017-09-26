67a1fc35a51610567617dcb91e9b7ba7f67cfb3d
jtavernor <jtavernor79@gmail.com>
2017-09-26 19:07:41 +0100

    updating CSS from bootstrap to my own CSS

--- index.php
+++ index.php
@@ -2,43 +2,44 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="CSS/personalPage.css">
     <title>James Tavernor</title>
 </head>
-    <?php
-        include 'header.html';
-    ?>
+<?php
+include 'header.html';
+?>
 <body>
 <div class="container">
-    <h1 class="text-center">Welcome to my website</h1>
-    <div class="row">
-        <div class="col-md-4">
-            <a href="about.php" class="thumbnail">
-                <img src="images/PersonalPicture.png" class="img-responsive img-rounded">
-                <div class="caption text-center">
+    <h1 class="text-centre">Welcome to my website</h1>
+    <div class="rowTilesConstantHeight">
+        <div class="col tiles">
+            <div class="captionedImage text-centre">
+                <a href="about.php">
+                    <img src="images/PersonalPicture.png" class="image-resize image-round">
                     <h4>About me</h4>
                     <p>Find out more about me</p>
-                </div>
-            </a>
+                </a>
+            </div>
         </div>
-        <div class="col-md-4">
-            <a href="publicCV.pdf" class="thumbnail">
-                <img src="images/Maths.png" class="img-responsive img-rounded">
-                <div class="caption text-center">
+        <div class="col tiles">
+            <div class="captionedImage text-centre">
+                <a href="publicCV.pdf">
+                    <img src="images/Maths.png" class="image-resize image-round">
                     <h4>My Qualifications</h4>
                     <p>Read my qualifications/CV</p>
-                </div>
-            </a>
+                </a>
+            </div>
         </div>
-        <div class="col-md-4">
-            <a href="projects.php" class="thumbnail">
-                <img src="images/Code.png" class="img-responsive img-rounded">
-                <div class="caption text-center">
+        <div class="col tiles">
+            <div class="captionedImage text-centre">
+                <a href="projects.php" class="thumbnail">
+                    <img src="images/Code.png" class="image-resize image-round">
                     <h4>My Projects</h4>
                     <p>View my previous projects</p>
-                </div>
-            </a>
+                </a>
+            </div>
         </div>
+
     </div>
 </div>
 </body>
